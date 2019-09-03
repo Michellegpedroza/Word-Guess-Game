c4be7b127e882d8a0f653309c3ac1ad174eadeb1
Michelle Gonzalez <m.chellegpedroza@gmail.com>
2019-09-03 14:07:33 -0700

    wrote the reset function for when the game is either won or lost

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,32 +1,58 @@
+// Array of Words
 const words = [`astronomy`, `comet`, `stars`, ` space`, `planets`, `moon`, `sun`, `jupiter`, `mercury`, `mars`, `earth`, `nasa`, `neptune`, `venus`, `saturn`, `uranus`, `light year`, `orbit`, `telescope`, `sky`, `solar`, `satellite`, `supernova`, `solstice`, `orbit`, `interstellar`, `gravity`, `galaxy`, `asteroid`]
 
+//Gets the Random Word 
 const getRandWord = function () {
   return words[Math.floor(Math.random() * words.length)]
 }
 
+//Starting Values
 let wins = 0
 let losses = 0
-let guesses = 5
-const lettersGuessed = []
+let guesses = 7
+let lettersGuessed = []
 let word = getRandWord()
 
+//Resets Game after win or lose
+const reset = function () {
+  word = getRandWord()
+  lettersGuessed = []
+  guesses = 7
+  displayWord()
+  document.getElementById(`guessesLeft`).textContent = guesses
+  document.getElementById(`wins`).textContent = wins
+  document.getElementById(`losses`).textContent = losses
+  document.getElementById(`letters`).textContent = lettersGuessed.join(`, `)
+}
+
+//Displays the word with _ 
 const displayWord = function () {
-  let wordstr = ``
+  let wordstr = `` 
+
+  // Tracks if the user has guess right
   let winStatus = true
+
+  //Loops to build string
   word.split(``).forEach(function (letter) {
     if (lettersGuessed.indexOf(letter) !== -1) {
       wordstr += `${letter} `
     } else {
+      //If letter guess incorrect display _
       wordstr += `_ `
       winStatus = false
     }
   })
-  if (winStatus) {
-    alert(`You Won!`)
-  }
   document.getElementById(`word`).textContent = wordstr
+
+//If word guess Right - Win alert and game Resets
+  if (winStatus) {
+    alert(`You Won! The word was ${word}!`)
+    wins++
+    reset()
+  }
 }
 
+//
 document.onkeyup = function (event) {
   if (event.keyCode >= 65 && event.keyCode <= 90) {
     if (lettersGuessed.indexOf(event.key) === -1) {
@@ -36,8 +62,11 @@ document.onkeyup = function (event) {
         displayWord()
       } else {
         guesses--
+        document.getElementById(`guessesLeft`).textContent = guesses
         if (guesses <= 0) {
-          alert(`You Lost!`)
+          alert(`You Lost! The word wasy ${word}`)
+          losses++
+          reset()
         }
       }
     }
