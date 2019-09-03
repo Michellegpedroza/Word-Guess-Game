92b090b8ad9efbdbd43479d379c23d826d032eda
Michelle Gonzalez <m.chellegpedroza@gmail.com>
2019-09-02 17:49:20 -0700

    adding the onkey functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,27 @@
-const words = [`astronomy`, `comet`, `stars`, ` space`, `planets`, `moon`, `sun`, `jupiter`, `mercury`, `mars`, `earth`, `nasa`, `neptune`, `venus`, `saturn`, `uranus`, `light year`, `orbit`, `telescope`, `sky`, `shooting star`, `solar`, `satellite`, `solar system`, `supernova`, `solstice`, `orbit`, `interstellar`, `gravity`, `galaxy`, `black hole`, `asteroid`]
+const words = [`astronomy`, `comet`, `stars`, ` space`, `planets`, `moon`, `sun`, `jupiter`, `mercury`, `mars`, `earth`, `nasa`, `neptune`, `venus`, `saturn`, `uranus`, `light year`, `orbit`, `telescope`, `sky`, `solar`, `satellite`, `supernova`, `solstice`, `orbit`, `interstellar`, `gravity`, `galaxy`, `asteroid`]
+
+
+const getRandWord = function () {
+  return words[Math.floor(Math.random() * words.length)]
+}
 
 let wins = 0
 let loses = 0
 let guesses = 5
 const guessedLetters = []
 
-const getRandWord = function () {
-  return words[Math.floor(Math.random() * words.length)]
-}
+let word = getRandWord()
 
 const displayWord = function () {
-  const word = getRandWord()
   let wordStr = ` `
   word.split(` `).forEach(function (letter) {
     wordStr += `_ `
   })
   document.getElementById(`word`).textContent = wordStr
-
 }
 
+document.onkeyup = function (event) {
+    if (event.keyCode >= 65 && event.keyCode <= 90){
 
+    }
+}
