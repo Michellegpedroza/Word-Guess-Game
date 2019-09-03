72f3d0cec4e2577426f2723c27fc387e0376576f
Michelle Gonzalez <m.chellegpedroza@gmail.com>
2019-09-02 17:19:24 -0700

    created my words array in JavaScript file and added variables for wins, loses, etc.

---
+++ assets/javascript/game.js
@@ -0,0 +1,6 @@
+const wordArr = [`astronomy`, `comet`, `stars`, ` space`, `planets`, `moon`, `sun`, `jupiter`, `mercury`, `mars`, `earth`, `nasa`, `neptune`, `venus`, `saturn`, `uranus`, `light year`, `orbit`, `telescope`, `sky`, `shooting star`, `solar`, `satellite`, `solar system`, `supernova`, `solstice`, `orbit`, `interstellar`, `gravity`, `galaxy`, `black hole`, `asteroid`]
+
+let wins = 0
+let loses = 0
+let guesses = 5
+const guessedLetters = []
