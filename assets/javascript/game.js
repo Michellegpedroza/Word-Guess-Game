5c0b67f06a5f961b423ac9e153427843918a8254
Michelle Gonzalez <m.chellegpedroza@gmail.com>
2019-09-02 17:31:49 -0700

    JavaScript for the word dashes & getting a random word

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,22 @@
-const wordArr = [`astronomy`, `comet`, `stars`, ` space`, `planets`, `moon`, `sun`, `jupiter`, `mercury`, `mars`, `earth`, `nasa`, `neptune`, `venus`, `saturn`, `uranus`, `light year`, `orbit`, `telescope`, `sky`, `shooting star`, `solar`, `satellite`, `solar system`, `supernova`, `solstice`, `orbit`, `interstellar`, `gravity`, `galaxy`, `black hole`, `asteroid`]
+const words = [`astronomy`, `comet`, `stars`, ` space`, `planets`, `moon`, `sun`, `jupiter`, `mercury`, `mars`, `earth`, `nasa`, `neptune`, `venus`, `saturn`, `uranus`, `light year`, `orbit`, `telescope`, `sky`, `shooting star`, `solar`, `satellite`, `solar system`, `supernova`, `solstice`, `orbit`, `interstellar`, `gravity`, `galaxy`, `black hole`, `asteroid`]
 
 let wins = 0
 let loses = 0
 let guesses = 5
 const guessedLetters = []
+
+const getRandWord = function () {
+  return words[Math.floor(Math.random() * words.length)]
+}
+
+const displayWord = function () {
+  const word = getRandWord()
+  let wordStr = ` `
+  word.split(` `).forEach(function (letter) {
+    wordStr += `_ `
+  })
+  document.getElementById(`word`).textContent = wordStr
+
+}
+
+
